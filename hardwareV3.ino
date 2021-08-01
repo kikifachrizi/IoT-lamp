a75c347cfca09562ad9a53f2bb6e1f15c6fda4da
kikifachrizi <40603810+kikifachrizi@users.noreply.github.com>
2021-08-02 06:44:36 +0700

    Add files via upload

---
+++ hardwareV3.ino
@@ -0,0 +1,63 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
+#include <WiFiClient.h>
+//#define ledd D4
+
+int ledd = 16;//D4
+int led = 2;
+String key = "1";
+
+WiFiClient wifiClient;
+
+
+void setup() {
+  pinMode(led, OUTPUT);
+  pinMode(ledd, OUTPUT);
+  const char* ssid = "ARKAN 3"; //Nama WiFi
+  const char* password = "faizarka123"; //Password WiFi
+//    const char* ssid = "Redmi Note 10"; //Nama WiFi
+//  const char* password = "12345678"; //Password WiFi
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting..");
+  }
+  Serial.println("Connection OK");
+}
+
+void loop() {
+
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+//    http.begin(wifiClient, "http://192.168.18.8/IoTlampu/get.php");
+http.begin(wifiClient, "http://213.190.4.239/iotlampu/get.php");
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    int httpResponseCode = http.POST("token="+key); 
+    delay(100);
+    if (httpResponseCode > 0) {
+      char json[500];
+      String response = http.getString();
+      response.toCharArray(json, 500);
+      StaticJsonDocument<200> doc;
+      deserializeJson(doc, json);
+      int light = doc["lampu"];
+      Serial.print("light = ");
+      Serial.println(light);
+//      Serial.println(response); //menampilkan respon dari website
+      if(light == 1){
+        digitalWrite(led, HIGH);
+      } else{
+        digitalWrite(led, LOW);
+      }
+    } else {
+      Serial.print("Error on sending POST: ");
+      Serial.println(httpResponseCode);
+    }
+    http.end();  //Free resources
+  } else {
+    Serial.println("Error in WiFi connection");
+  }
+  delay(100);
+}
